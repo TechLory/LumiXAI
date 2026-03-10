9ac917adfee458e4938b18c690f644478c547f88
Lorenzo <lorenzogatta.lg@gmail.com>
2026-03-10 18:29:25 +0100

    fix: navbar links

--- frontend/app/components/layout/Navbar.tsx
+++ frontend/app/components/layout/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
     <nav className="w-full bg-yellow-600 text-black px-10 py-2 mb-2 flex justify-between font-mono font-semibold">
       <div className="select-none">LumiXAI</div>
       <div className="flex gap-10">
-        <Link className="hover:underline underline-offset-4 decoration-2" href={""}>Docs</Link>
-        <Link className="hover:underline underline-offset-4 decoration-2" href={""}>GitHub</Link>
+        <Link className="hover:underline underline-offset-4 decoration-2" href={"http://localhost:8001"} target="_blank">Docs</Link>
+        <Link className="hover:underline underline-offset-4 decoration-2" href={"https://github.com/TechLory/xai-framework-lorenzo-gatta"} target="_blank">GitHub</Link>
       </div>
     </nav>
   );
